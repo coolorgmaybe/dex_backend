d686c37568aded02b825999ff14ae428bbf23623
Ilya Oskin <ilya.arcadich@gmail.com>
2019-04-07 12:21:28 +0300

    TargetOrder id added.

--- src/main/scala/dex_backend/trading/MatchResult.scala
+++ src/main/scala/dex_backend/trading/MatchResult.scala
@@ -2,20 +2,20 @@ package dex_backend.trading
 
 import dex_backend.trading.order.{Order, OrderDirection}
 
-final case class MatchResult(orderToMeet: Order,
+final case class MatchResult(askOrder: Order,
                              correspondingOrders: List[Order],
                              partiallyExecutedOrderOpt: Option[Order]) {
 
-  def toSellerMostDirectives: List[TradeDirective] = orderToMeet.direction match {
+  def toSellerMostDirectives: List[TradeDirective] = askOrder.direction match {
     case OrderDirection.Sell => correspondingOrders.map { order =>
-      val priceToExecute = math.max(orderToMeet.price, order.price)
-      TradeDirective(orderToMeet.clientId, order.clientId, orderToMeet.assetId,
-        orderToMeet.volume, priceToExecute, orderToMeet)
+      val priceToExecute = math.max(askOrder.price, order.price)
+      TradeDirective(askOrder.clientId, order.clientId, askOrder.assetId,
+        askOrder.volume, priceToExecute, askOrder.id)
     }
     case OrderDirection.Buy => correspondingOrders.map { order =>
-      val priceToExecute = math.min(orderToMeet.price, order.price)
-      TradeDirective(order.clientId, orderToMeet.clientId, order.assetId,
-        order.volume, priceToExecute, orderToMeet)
+      val priceToExecute = math.min(askOrder.price, order.price)
+      TradeDirective(order.clientId, askOrder.clientId, order.assetId,
+        order.volume, priceToExecute, askOrder.id)
     }
   }
 
--- src/main/scala/dex_backend/trading/TradeDirective.scala
+++ src/main/scala/dex_backend/trading/TradeDirective.scala
@@ -1,10 +1,8 @@
 package dex_backend.trading
 
-import dex_backend.trading.order.Order
-
 final case class TradeDirective(targetOrderOwnerClientId: String,
                                 counterPartyClientId: String,
                                 assetId: String,
                                 volume: Long,
                                 price: Long,
-                                targetOrder: Order)
+                                targetOrderId: Int)
